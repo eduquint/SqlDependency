42e35f6d3e90889a70ae65257689d4695af771a2
eduar <eduar@DESKTOP-FT5E8N8>
2016-07-27 17:01:28 -0300

    Add project files.

---
+++ SqlDependency/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SqlDependency
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 570);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(106, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Changes occurred: 0";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(808, 565);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(98, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Get Contacts";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(725, 569);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(77, 17);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Re-register";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(894, 547);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(918, 600);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "SQL Query Notification";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
---
+++ SqlDependency/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SqlDependency.Properties;
+
+namespace SqlDependency {
+	public partial class Form1 : Form {
+
+		#region Protected Vars
+
+		protected int ChangeCount;
+		protected const string TableName = "PréBoleto";
+		protected const string StatusMessage = "Changes occurred:";
+		protected bool ExitRequested = false;
+		protected bool WaitInProgress;
+
+		// The following objects are reused
+		// for the lifetime of the application.
+		protected DataSet DataToWatch;
+		protected SqlConnection Connection;
+		protected SqlCommand Command;
+
+		// The Service Name is required to correctly  register for notification.
+		// The Service Name must be already defined with Service Broker for the database you are querying.
+		protected const string ServiceName = "SbmDashboardChangeNotifications";
+
+		protected const string QueueName = "SbmDashboardChangeMessages";
+
+		// The database name is needed for both the connection string and the SqlNotificationRequest.Options property.
+		protected const string DatabaseName = "SBM With Users";
+
+
+		// Specify how long the notification request should wait before timing out.
+		// This value waits for 30 seconds. 
+		protected int NotificationTimeout = 30;
+
+		#endregion
+
+		#region Constructor/Destructor
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e) => Connection?.Close();
+
+		#endregion
+
+		#region Event Processing Methods
+
+		private void button1_Click(object sender, EventArgs e) {
+			ChangeCount = 0;
+			label1.Text = $"{StatusMessage} {ChangeCount}";
+
+			if (Connection == null) {
+				Connection = new SqlConnection($"Data Source=(local);Integrated Security=true;Initial Catalog={DatabaseName};Pooling=False;Asynchronous Processing=true;");
+			}
+
+			if (Command == null) {
+				Command = new SqlCommand(Resources.StoreProcedure, Connection);
+			}
+
+			if (DataToWatch == null) {
+				DataToWatch = new DataSet();
+			}
+
+			button1.Enabled = !checkBox1.Checked;
+
+			GetData(checkBox1.Checked);
+		}
+
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e) => button1.Enabled = !checkBox1.Checked || button1.Enabled;
+
+		#endregion
+
+		#region Supporting Methods
+
+		private void GetData(bool register) {
+			// Empty the DataSet so that there is only one batch of data displayed.
+			DataToWatch.Clear();
+
+			// Make sure the command object does not already have	a notification object associated with it.
+			Command.Notification = null;
+
+			if (register) {
+				// Create and bind the SqlNotificationRequest object to the command object.
+				// If a time-out occurs, a notification will indicating that is the reason for the notification.
+				var request = new SqlNotificationRequest {
+																									 UserData = new Guid().ToString(),
+																									 Options = $"Service={ServiceName};local database={DatabaseName}",
+																									 Timeout = NotificationTimeout
+																								 };
+
+				Command.Notification = request;
+			}
+
+			using (var adapter = new SqlDataAdapter(Command)) {
+				adapter.Fill(DataToWatch, TableName);
+
+				dataGridView1.DataSource = DataToWatch;
+				dataGridView1.DataMember = TableName;
+			}
+
+			if (register) {
+				// Start the background listener.
+				Listen();
+			}
+		}
+
+		private void Listen() {
+			using (var sqlCommand = new SqlCommand($"WAITFOR (RECEIVE * FROM {QueueName});", Connection)) {
+				if (Connection.State != ConnectionState.Open) {
+					Connection.Open();
+				}
+
+				// Make sure we don't time out before the notification request times out.
+				sqlCommand.CommandTimeout = NotificationTimeout + 120;
+
+				AsyncCallback callBack = OnReaderComplete;
+				var asynResult = sqlCommand.BeginExecuteReader(callBack, sqlCommand);
+
+				if (asynResult.IsCompleted) {
+					WaitInProgress = true;
+				}
+			}
+		}
+
+		private void OnReaderComplete(IAsyncResult asynResult) {
+			// You may not interact with the form and its contents from a different thread, and this callback procedure
+			// is all but guaranteed to be running from a different thread than the form. 
+			// Therefore you cannot simply call code that updates the UI.
+			// Instead, you must call the procedure from the form's thread.
+			// This code will use recursion to switch from the thread pool to the UI thread.
+			if (InvokeRequired) {
+				AsyncCallback switchThreads = OnReaderComplete;
+				object[] args = {
+													asynResult
+												};
+				BeginInvoke(switchThreads, args);
+				return;
+			}
+
+			// At this point, this code will run on the UI thread.
+			try {
+				WaitInProgress = false;
+				var reader = ((SqlCommand) asynResult.AsyncState).EndExecuteReader(asynResult);
+
+				while (reader.Read()) {
+					// Empty queue of messages.
+					// Application logic could parse the queue data to determine why things.
+					for (var i = 0; i <= reader.FieldCount - 1; i++)
+						Debug.WriteLine(reader[i].ToString());
+				}
+
+				reader.Close();
+				ChangeCount += 1;
+				label1.Text = $"{StatusMessage} {ChangeCount}";
+				Application.DoEvents();
+
+				// The user can decide to request	a new notification by	checking the CheckBox on the form.
+				// However, if the user has requested to exit, we need to do that instead.
+				if (ExitRequested) {
+					Close();
+				}
+				else {
+					GetData(checkBox1.Checked);
+				}
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		#endregion
+	}
+}
